681bec87be89b4757f50a86e8f4d0cf47f5bdd6b
Александр <fedoseevale@gmail.com>
2022-11-27 22:02:10 +0400

    Execise 3 done+

--- Execise3.py
+++ Execise3.py
@@ -2,26 +2,37 @@
 # Реализуйте алгоритм перемешивания списка. Встроенный алгоритм SHUFFLE не использовать! Реализовать свой метод
 import random
 
-arraySize = int(input('Введите размер массива: '))
-array = []
+my_listSize = int(input('Введите размер списка: '))
+my_list = []
 
-for i in range(arraySize):
-    array.append(random.randint(0, 100))
-print(f'Имеется массив: {array}')
+for i in range(my_listSize):
+    my_list.append(random.randint(0, 100))
+print(f'Имеется список: {my_list}')
 
 # Вариант 1:
-for i in range(arraySize // 2):
-    obj = array[i]
-    array[i] = array[arraySize - 1 - i]
-    array[arraySize - 1 - i] = obj
-print(f'Перемешанный массив выглядит следующим образом: {array}')
+for i in range(my_listSize // 2):
+    obj = my_list[i]
+    my_list[i] = my_list[my_listSize - 1 - i]
+    my_list[my_listSize - 1 - i] = obj
+print(f'Перемешанный список выглядит следующим образом: {my_list}')
 
 # Вариант 2:
-for i in range(arraySize // 2):
-    obj = array[i+1]
-    array[i+1] = array[arraySize - 1 - i]
-    array[arraySize - 1 - i] = obj
-print(f'Перемеанный массив вышлядит следующим образом: {array}')
+for i in range(my_listSize // 2):
+    obj = my_list[i+1]
+    my_list[i+1] = my_list[my_listSize - 1 - i]
+    my_list[my_listSize - 1 - i] = obj
+print(f'Перемешанный список выглядит следующим образом: {my_list}')
 
+# Можно реализовать через метод:
+def my_listChange(mylist: list):
+    size = len(mylist)
+    for i in range(size // 2):
+        obj = mylist[i]
+        mylist[i] = mylist[size - 1 - i]
+        mylist[size - 1 - i] = obj
+    return mylist
 
-# Могут быть и другие варианты
+my_listNew = my_listChange(my_list)
+print(f'Перемешанный список выглядит следующим образом: {my_listNew}')
+
+# Могут быть реализованы и другие варианты
